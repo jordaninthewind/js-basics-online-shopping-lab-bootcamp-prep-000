f16ac962d5782c611bbaf8ab25878371b99adcf7
jordaninthewind <jordan.kline@gmail.com>
2017-11-24 23:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ function viewCart() {
       var keyName = Object.keys(cart[i]);
       var value = cart[i][keyName];
       var itemString = `${keyName} at $${value},`;
-        if (i === cart.length - 2) {
+        if (i === cart.length - 1) {
           returnString = returnString + ' and ';
-        } if (i === cart.length - 1) {
+        } if (i === cart.length) {
           itemString = itemString - ',';
         }
         returnString = returnString + itemString;
