2fe71cbe5e210b735eb0a66697ff1785f1672fd8
jordaninthewind <jordan.kline@gmail.com>
2017-11-24 23:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addToCart(itemName) {
   return cart;
 }
 
-function viewCart(cart) {
+function viewCart() {
   // Empty Cart Return 'Your shopping cart is empty.'
   if (cart.length == 0) {
     console.log('Your shopping cart is empty.');
