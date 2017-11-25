60d27e008e13914d14ff401b776c958af284066e
jordaninthewind <jordan.kline@gmail.com>
2017-11-25 00:10:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
       var itemString = ` ${keyName} at $${value},`;
         if (i < cart.length) {
           returnString = returnString + itemString;
-        } else if (i === cart.length - 1)
+        } else {
           returnString = returnString + " and " + itemString
         }
     returnString = returnString.slice(0, -1);
